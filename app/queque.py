45c40bd0f1cb2808679f8a969211f50e35c991fe
Alekao18 <umzehonorio@gmail.com>
2025-06-21 20:05:49 -0300

    Começando a montar o main

--- app/queque.py
+++ app/queque.py
@@ -1,5 +1,5 @@
-from app.models import Mensagem
-from app.datbase import r
+from models import Mensagem
+from datbase import r
 import json
 import uuid
 
@@ -13,10 +13,22 @@ def adicionar_fila(usuario: str, conteudo: str):
         status = "pendente"
     )
     r.rpush(QUEUE_ID, msg.model_dump_json())
+
 def proxima_mensagem():
     for i in range(r.llen(QUEUE_ID)):
         linha = r.lindex(QUEUE_ID, i)
         msg_1 = Mensagem.model_validate_json(linha)
         if msg_1.status == "pendente":
             return msg_1
+    return None
+
+def atribuir_mensagem(msg_id: str, atendente: str):
+    for i in range(r.llen(QUEUE_ID)):
+        linha = r.lindex(QUEUE_ID, i)
+        msg_2 = Mensagem.model_validate_json(linha)
+        if msg_2.id == msg_id:
+            msg_2.status = "sendo atendido por {atendente}"
+            msg_2.atendente = atendente
+            r.lset(QUEUE_ID, i, msg_2.model_dump_json())
+            return msg_2
     return None
