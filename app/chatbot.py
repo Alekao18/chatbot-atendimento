f39768ab71dd895077ab55b9db050f41aea204fc
Alekao18 <umzehonorio@gmail.com>
2025-06-24 10:51:38 -0300

    Ainda em fase de teste

--- app/chatbot.py
+++ app/chatbot.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from app.queque import adicionar_fila, proxima_mensagem, atribuir_mensagem
 
 app = FastAPI()
+app.router
 
 class Entrada(BaseModel):
     usuario : str
@@ -11,17 +12,19 @@ class Atendente(BaseModel):
     msg_id: str
     atendente: str
 
-app.post("/mensagem")
+@app.post("/mensagem")
 def nova_mensagem(dados: Entrada):
     adicionar_fila(dados.usuario, dados.conteudo)
     return {"status": "Mensagem recebida!"}
-app.get("/fila")
+
+@app.get("/fila")
 def ver_fila():
     msg = proxima_mensagem()
     if msg:
         return msg
     return {"status": "Fila vazia"}
-app.post("/atender")
+
+@app.post("/atender")
 def atender(dados: Atendente):
     msg = atribuir_mensagem(dados.msg_id, dados.atendente)
     if msg:
