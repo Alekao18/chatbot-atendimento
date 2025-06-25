b55bb9d8ac768a4006001fc26e022966025eac17
Alekao18 <umzehonorio@gmail.com>
2025-06-25 20:25:29 -0300

    colocando banco da dados na nuvem

--- app/datbase.py
+++ app/datbase.py
@@ -1,6 +1,14 @@
 import redis 
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-r = redis.Redis.from_url(os.getenv('REDIS_URL'), decode_responses=True)
+
+import redis
+
+r = redis.Redis(
+    host='redis-12006.c345.southamerica-east1-1.gce.redns.redis-cloud.com',
+    port=12006,
+    username='usrchatbot',  # ou o que estiver configurado
+    password='Usrchatbot25@',  # substitua pela senha criada
+    ssl=True
+)
+
+
